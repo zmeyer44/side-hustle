8c01999a47373534f76e824f1e57e8aa2f7a3008
zmeyer44 <meyerz@wustl.edu>
2020-04-28 13:45:56 -0400

    Updates

---
+++ precache-manifest.c8978f12a4098f7245c527c9a879413b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e10b738f89a9f60e261f81777cf8ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfb7eb039011dfd803b8",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "43967480790df8dd98be",
+    "url": "/static/css/main.67e061ba.chunk.css"
+  },
+  {
+    "revision": "dfb7eb039011dfd803b8",
+    "url": "/static/js/2.31510a58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31510a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43967480790df8dd98be",
+    "url": "/static/js/main.bba50e74.chunk.js"
+  },
+  {
+    "revision": "8a6a71069fe6546955b4",
+    "url": "/static/js/runtime-main.92aa17fe.js"
+  },
+  {
+    "revision": "350b200bc75072261be1b6ba6098520d",
+    "url": "/static/media/logo.350b200b.svg"
+  }
+]);
